d2af2168df18314d13728651eb82a905b539e01f
github username <github email address>
2018-05-12 17:01:16 -0400

    Done.

--- application.rb
+++ application.rb
@@ -2,9 +2,11 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+    resp.write "Hello, my name is Abdoul Diallo. "
+    resp.write "I'm an aspiring Software Engineer. "
+    resp.write "I dream to one day use the skills I acquire to contribute towards making the world a better place, for all. "
+    resp.write "WasSalaam"
     resp.finish
   end
 
 end
-
